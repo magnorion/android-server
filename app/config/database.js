76d7e0f9544ec7cf09923b9f5f11820458d61722
Lucas Dias Lopes <lucas.lopes@unicid.edu.br>
2019-01-11 10:45:14 -0200

    add conexao com o mongo atlas cloud

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,9 @@
 module.exports = (app) => {
     const mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost:27017/android', { useNewUrlParser: true });
+    mongoose.connect(
+        'mongodb+srv://magno:magno@appcluster-a9qxu.mongodb.net/test?retryWrites=true', 
+        { useNewUrlParser: true }
+    );
 
     return mongoose;
 }
